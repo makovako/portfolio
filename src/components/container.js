88fc8e1d60ef57d39b70ab686112b22d798775ac
Marek Valko <valko.marek@gmail.com>
2020-02-17 20:35:41 +0100

    fix fot big font awesome icons on startup

--- src/components/container.js
+++ src/components/container.js
@@ -3,6 +3,13 @@ import styles from "./container.module.css"
 import Navigation from '../components/navigation'
 import Footer from '../components/footer'
 import Header from './header'
+
+// fix for big font awesome icons
+import { config } from "@fortawesome/fontawesome-svg-core"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+
+config.autoAddCss = false
+
 export default ({ children }) => (
   <div className={styles.container}>
     <Header></Header>
