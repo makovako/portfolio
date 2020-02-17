50f5ff2467afd15743b226a7faf62a79125b4e3b
Marek Valko <valko.marek@gmail.com>
2020-02-17 16:59:35 +0100

    main pages

---
+++ src/components/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './about.module.css'
+
+export default () => (
+    <main>
+        <h2>About</h2>
+        <p>This website shows some of my projects. </p>
+    </main>
+)
+
---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Container from '../components/container'
+import About from '../components/about'
+
+export default () => (
+    <Container>
+        <About />
+    </Container>
+)
